e1974b5ae89d44628f7eac99d4f42b8ba6f4acd1
Rachel Connolly <rshconn@users.noreply.github.com>
2025-01-15 22:37:33 -0800

    add is_balanced question

---
+++ is_balanced/is_balanced_question.py
@@ -0,0 +1,15 @@
+# A bracket is considered to be any one of the following characters: (){}[].
+# Two brackets are considered to be a matched pair if the an opening bracket occurs to the left of a closing bracket of the exact same type.
+# There are three types of matched pairs of brackets: [], {}, and ().
+# Given a string, determine whether each sequence of brackets is balanced. If a string is balanced, return True. Otherwise, return False.
+
+# Example 1
+# Input: s = '{[()]}'
+# Output: True
+
+# Example 2
+# Input: s = '{[(])}'
+# Output: False
+
+def is_balanced(s: str) -> bool:
+	pass
