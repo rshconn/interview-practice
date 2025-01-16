a3be5a5a234a1d279ebf3d370a2dca64244db739
Rachel Connolly <rshconn@users.noreply.github.com>
2025-01-15 22:29:13 -0800

    add find_longest question

---
+++ find_longest/find_longest_question.py
@@ -0,0 +1,8 @@
+# Given a lowercase string s, find the length of the longest substring that does not contain any vowels.
+
+# Example
+# Input: s = 'helloworld'
+# Output: 3
+
+def find_longest(s: str) -> int:
+  pass
