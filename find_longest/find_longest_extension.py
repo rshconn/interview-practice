2d8ec5b8dba5894dedb0fbdc251a8e73b7f3fa35
Rachel Connolly <rshconn@users.noreply.github.com>
2025-01-15 22:34:51 -0800

    add find_longest extension

---
+++ find_longest/find_longest_extension.py
@@ -0,0 +1,5 @@
+# Given a string s, return the longest possible substring that has exactly k unique characters. 
+# If there is more than one substring of longest possible length, then return any of them.
+
+def find_longest_substring(s: str, k: int) -> str:
+  pass
