27e7577d4ba3d6c40e6a83d200174a17004f0ad4
Rachel Connolly <rshconn@users.noreply.github.com>
2025-05-29 00:13:29 +0200

    add daily_temps_question.py

---
+++ daily_temps/daily_temps_question.py
@@ -0,0 +1,10 @@
+# Given a list of daily temperatures temperatures, return a list such that, for each day in the input, tells you how many days you would have to wait until a warmer temperature. 
+# If there is no future day for which this is possible, put 0 instead.
+
+# Example 1
+# Input: [73, 74, 75, 71, 69, 72, 76, 73]
+# Output: [1, 1, 4, 2, 1, 1, 0, 0]
+
+# Example 2
+# Input: [75, 74, 73, 72]
+# Output: [0, 0, 0, 0]
