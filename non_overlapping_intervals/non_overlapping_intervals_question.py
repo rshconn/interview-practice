164a3ff6a8a09867d17823a48b8f17259878aaf8
Rachel Connolly <rshconn@users.noreply.github.com>
2025-06-06 15:58:41 +0300

    add non_overlapping_intervals_question.py

---
+++ non_overlapping_intervals/non_overlapping_intervals_question.py
@@ -0,0 +1,13 @@
+# Given an array of intervals intervals, return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+# Intervals like [1,2] and [2,3] have borders ”touching” but they don’t overlap each other.
+
+# Example 1
+# intervals = [[1,2], [2,3], [3,4], [1,3]]
+# Output: 1
+
+# Example 2
+# intervals = [[1,2], [1,2], [1,2]]
+# Output: 2
+
+def remove_intervals(intervals: list) -> int:
+  pass
