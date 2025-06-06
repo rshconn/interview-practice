f59297cc654be871adc6c6f137bb52d791d4201a
Rachel Connolly <rshconn@users.noreply.github.com>
2025-06-06 16:17:44 +0300

    add saddle_point_question.py

---
+++ saddle_point/saddle_point_question.py
@@ -0,0 +1,13 @@
+# Given a matrix of n x n size, find the saddle point of the matrix. 
+# A saddle point is an element of the matrix such that it is the minimum element in its row and the maximum in its column. 
+
+# Example 1
+# Input: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# Output: 7
+
+# Example 2
+# Input: [[1, 2, 3], [4, 5, 6], [10, 18, 4]]
+# Output: None
+
+def find_saddle_point(matrix):
+  pass
